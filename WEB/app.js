9029845d500fdf27c759af4be5168d0befc50f7e
TAEYONG KIM <amico741@naver.com>
2022-02-16 00:20:32 +0900

    채팅 설정

--- WEB/app.js
+++ WEB/app.js
@@ -2,7 +2,18 @@ const express = require('express');
 
 const app = express();
 
-app.set('port', process.env.PORT || 3000)
+/* by Taeyong npm i ws pug 할 것!*/
+
+app.set("view engine", "pug"); // view engine pug로 지정
+app.set("views", __dirname + "/views"); // views 실행중인 폴더 /views
+app.use("/public", express.static(__dirname + "/public"));
+app.set('port', process.env.PORT || 3000);
+
+app.get("/", (req, res) => {
+    res.render("home"); // home.pug
+});
+app.get("/*", (_, res) => res.redirect("/"));
+/* by Taeyong */
 
 app.listen(app.get('port'), () => {
     console.log(app.get('port'), '번 포트에서 대기 중');
