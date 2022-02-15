9e56803e8ffc96d72aa27e23e275b5be4c891341
khsexk <kohyunsuk98@gmail.com>
2022-02-15 22:54:12 +0900

    원복

---
+++ WEB/app.js
@@ -0,0 +1,9 @@
+const express = require('express');
+
+const app = express();
+
+app.set('port', process.env.PORT || 3000)
+
+app.listen(app.get('port'), () => {
+    console.log(app.get('port'), '번 포트에서 대기 중');
+});
