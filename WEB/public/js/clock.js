bd282e93df8f610118dfe6e14f48d8891d368fc3
TAEYONG KIM <amico741@naver.com>
2022-02-18 01:07:24 +0900

    홈페이지 꾸미기

---
+++ WEB/public/js/clock.js
@@ -0,0 +1,15 @@
+const clock = document.querySelector("#clock");
+const header = clock.querySelector("header");
+
+
+function handleClock() {
+    const date = new Date();
+    const hours =  String(date.getHours()).padStart(2, "0");
+    const minutes = String(date.getMinutes()).padStart(2, "0");
+    const seconds = String(date.getSeconds()).padStart(2, "0");
+
+    header.innerText = `${hours}:${minutes}:${seconds}`;
+}
+
+handleClock();
+setInterval(handleClock, 1000);
